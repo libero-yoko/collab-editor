de261a61df02e7a691dd59bde3f32093fa29dca5
YokoK <7126966+libero-yoko@users.noreply.github.com>
2021-08-22 20:00:51 -0700

    Added delete end point

--- server/controllers/conversationsController.js
+++ server/controllers/conversationsController.js
@@ -20,11 +20,36 @@ conversationsController.getConversations = (req, res, next)=>{
       return next();
     }
   })
-  // .catch((err) => {
-  //   return next(err);
-  // })
 }
 
+conversationsController.deleteConversation = (req, res, next)=>{
+  console.log("deleting")
+  const {id} = req.param.id;
+  console.log(id)
+  if(!id){
+    const err = new Error('missing id');
+    err.statusCode = 400;
+    next(err);
+  }else{
 
+    collabEditors.remove({"conversationId":id})
+    .then(conversations => {
+      console.log(conversations.length)
+      if(conversations.length === 0){
+        const err = new Error('No data stored under the given Id');
+        err.statusCode = 400;
+        next(err);
+      }else{
+        res.locals.message = {
+          "msg": "Successfully Deleted" + conversations.ok + " Conversations",
+          "ok": true,
+      };
+        console.log(res.locals.message);
+        return next();
+     };
+    });
+  };
+};
 
 export default conversationsController;
+
--- server/routes/conversationsRouter.js
+++ server/routes/conversationsRouter.js
@@ -6,8 +6,9 @@ router.get('/', conversationsController.getConversations, (req, res)=>{
   res.status(200).send(res.locals.message);
 })
 
-router.delete('/', (req, res)=>{
-  res.status(200).send('delete conversasions');
+router.delete('/:id', conversationsController.deleteConversation, (req, res)=>{
+  console.log(res.locals.message)
+  res.status(204).send(res.locals.message);
 })
 
 export default router;
