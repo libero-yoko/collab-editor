05396baea53cbe948c89152199696d0fc97b7956
YokoK <7126966+libero-yoko@users.noreply.github.com>
2021-08-23 07:23:43 -0700

    Updated info

--- server/controllers/infoController.js
+++ server/controllers/infoController.js
@@ -14,8 +14,8 @@ infoController.getInfo = (req, res, next) => {
     "sources": "https://github.com/libero-yoko/collab-editor.git",
     "answers": {
       "1": "First I setup the backend environment with Node.js/Express/Webpack/MongoDB(MongoAtlas), then started working on the server settings, separating routers, models and controllers. After everything connected I worked on the actual logic of mutation in the mutationsController.js where you can see how the texts gets mutated. I used Postman to test locally, and setup AWS Lightsail to host the backend/frontend. I modified the Apache setteings in the config file so Apache redirects the trafic to the Node.js. Finally connected MongoAtlas to Lightsail.",
-      "2": "I probaly spent too much time on the configuration. I would love to start implementing the frontend",
-      "3": "I would probably reduce the volume of the work by 30%?"
+      "2": "I would like to continue working on the backend algorythm",
+      "3": "Since this was the significant amont of work, I would probably reduce the volume of the work by 30%."
     }
   }
 
